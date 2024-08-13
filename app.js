7f6d4406b7ab0942ec61a96d5dcc764113867574
My_simvol <my.simvol@gmai.com>
2024-08-13 18:17:19 +0400

    урок 4.2

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 
-let a = 'Hello, ';
+let a = 'Hello';
 let b = 'penis';
 
-console.log(a + b);
-a = 'chupa';
-b = 'chups';
+console.log(a + ', my name is ' + b);
+a = '13';
+b = '14';
 
-console.log(a+b);
+console.log(a + b + 5);
+/*
+принимает все зак конкатенацию то бишь склеивает цифры и переменные со словами
+*/
