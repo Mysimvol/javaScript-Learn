19d982a57b8f4a03c9d3c8fb8b8c965a5d0d4b69
My_simvol <my.simvol@gmai.com>
2024-08-13 22:22:08 +0400

    Упражнение 5.2

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const money = 100;
+let startDeposit = 12000;
+const rate = 0.07
+const oneYearDep = startDeposit * rate;
+console.log(oneYearDep);
 
-if(money > 90){
-    console.log('вы можете купить продукт');
-}   else if (money > 50) {
-    console.log('Можешь купить Доширака с говнядиной');
+const oneMothDep = oneYearDep / 12;
+console.log(oneMothDep);
+
+const endDeposit = startDeposit * (1 + rate / 12) ** 24;
+console.log(endDeposit)
+
+const housePrice = 13900
+
+if (endDeposit > housePrice){
+    console.log('Вы можете купить квартиру,\nостаток после покупки будет ' + (endDeposit - housePrice));
 }   else{
-    console.log('No money Бомж блят');
+    console.log('Продай почку')
 }
