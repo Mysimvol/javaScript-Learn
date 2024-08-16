91cb806e17070733c2d9ec59d83a4919159190a2
My_simvol <my.simvol@gmai.com>
2024-08-17 01:18:33 +0400

    8.2 countinue

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-for(let i = 1; i <= 5; i++){
-    console.log(`Ваш баланс ${i}$`)
+const tasks = ['Задача1','Задача2','Задача3','Задача4'];
+
+for(let i = 0; i < tasks.length; i++){
+    if(tasks[i] === 'Задача2'){
+        continue;
+    }
+    console.log(tasks[i]);
 }
