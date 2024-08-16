e9d875112b2944ae72c3688a3bcdea8062510476
My_simvol <my.simvol@gmai.com>
2024-08-17 00:53:02 +0400

    7.7 Упражнение по массивам

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-const userData = ['Антон', 18, 'Саратов',];
+const url = 'https://purpleschool.ru/course/javascript'
+const res = url.split('//')
+console.log(res)
 
-function getData(){
-    return ['Антон', 18, 'Саратов',];
+function massivUrl(arg){
+    const url = arg;
+    const result = url.split('/');
+    const [http, _, name, ...others] = result;
+    console.log(http, name, '/' + others.join('/'));
+    console.log(`Протокол: ${http}`);
+    console.log(`Доменное имя: ${name}`)
+    console.log(`Библиотека /${others.join('/')}`)
 }
-const [userName, _, city] = getData();
-
-console.log(userName, city);
+massivUrl('https://purpleschool.ru/course/javascript')
