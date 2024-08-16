c583c94a4e1ee726dfa59385ccaed13c3cf3df79
My_simvol <my.simvol@gmai.com>
2024-08-17 02:21:57 +0400

    8.6 цикл for of и for in

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
-// let i = 1;
 
-// while(i < 5){
-//     console.log(`вывод - ${i}`);
-//     i++;
-// }
+const arr = [1,4,5,2,5,'sdjgsl'];
 
-const arr = [1,4,5,2,5,7];
-
-let j = 0;
-while(arr[j] <= 4 && j < arr.length ){
-    console.log(arr[j]);
-    j++;
+for(let i = 0; i < arr.length; i++){
+    console.log(arr[i])
 }
 
-let k = 0
-do{
-    console.log(k)
-    k++
-}while(k < 0)
+for(let element of arr){
+    console.log(element)
+}
+
+for( let index in arr){
+    console.log(index)
+}
