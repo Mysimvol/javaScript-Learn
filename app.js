4c2462bde73a2b45e2a001014f6c49eb81a16e0a
My_simvol <my.simvol@gmai.com>
2024-08-17 00:12:30 +0400

    Урок 7.6 деструктуризация массива

--- app.js
+++ app.js
@@ -1,34 +1,8 @@
-const tasks = ['Задача1', 'Задача2', 'Задача3', 'Задача4'];
+const userData = ['Антон', 18, 'Саратов',];
 
-function addTask(arg){
-    return tasks.push(arg);
+function getData(){
+    return ['Антон', 18, 'Саратов',];
 }
+const [userName, _, city] = getData();
 
-addTask('piska')
-console.log(tasks)
-
-deleteTask('Задача1')
-function deleteTask(task){
-    const rel = tasks.indexOf(task);
-    // return console.log(rel);
-    if (rel >= 0 ){
-        tasks.splice(rel, 1);
-        console.log(tasks);
-    } else{
-        console.log('Такой задачи нет')
-    }
-}
-
-beginTask('Задача4')
-function beginTask(task){
-    const rel = tasks.indexOf(task);
-    if(rel >= 0 ){
-        const oldTask = tasks[rel];
-        tasks.splice(rel,1);
-        tasks.unshift(oldTask);
-        console.log(tasks)
-    }   else{
-        console.log('Не правильная задача')
-    }
-    return
-}
+console.log(userName, city);
